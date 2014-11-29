0c6f978009d643c96eb0e8661f595298fbc05a1f
felcm4 <felcm4@mst.edu>
2014-11-29 13:50:57 -0800

    Updated register library files and WIP scoreboard sources

--- register_lib.cpp
+++ register_lib.cpp
@@ -21,80 +21,18 @@
  ***********************************************/
 
 	void GLOB_OP_REGS::read_data(void){
-		//regSelect.read()
-		switch (regSelect.read()){
-		case 0:
-			opReg[0] = dataIn;
-			break;
+		sc_uint<3> index = regSelect.read();
+		opReg[index] = dataIn;
 
-		case 1:
-			opReg[1] = dataIn;
-			break;
-
-		case 2:
-			opReg[2] = dataIn;
-			break;
-
-		case 3:
-			opReg[3] = dataIn;
-			break;
-
-		case 4:
-			opReg[4] = dataIn;
-			break;
-
-		case 5:
-			opReg[5] = dataIn;
-			break;
-
-		case 6:
-			opReg[6] = dataIn;
-			break;
-
-		case 7:
-			opReg[7] = dataIn;
-			break;
-
-		}//end switch
-	}// end read_data
+	}//end read_data
 
 	void GLOB_OP_REGS::write_data(void){
-			//regSelect.read()
-			switch (regSelect.read()){
-			case 0:
-				dataOut.write(opReg[0]);
-				break;
+			sc_uint<3> index = regSelect.read();
+			dataOut.write(opReg[index]);
 
-			case 1:
-				dataOut.write(opReg[1]);
-				break;
+		}//end write_data
 
-			case 2:
-				dataOut.write(opReg[2]);
-				break;
 
-			case 3:
-				dataOut.write(opReg[3]);
-				break;
-
-			case 4:
-				dataOut.write(opReg[4]);
-				break;
-
-			case 5:
-				dataOut.write(opReg[5]);
-				break;
-
-			case 6:
-				dataOut.write(opReg[6]);
-				break;
-
-			case 7:
-				dataOut.write(opReg[7]);
-				break;
-
-			}//end switch
-		}// end write_data
 
 
 /************************************************
@@ -105,81 +43,17 @@
  * Modified:	N/A
  *
  ***********************************************/
+
 	void ADDRESS_REGS::read_data(void){
-		//regSelect.read()
-		switch (regSelect.read()){
-		case 0:
-			addReg[0] = dataIn;
-			break;
-
-		case 1:
-			addReg[1] = dataIn;
-			break;
-
-		case 2:
-			addReg[2] = dataIn;
-			break;
-
-		case 3:
-			addReg[3] = dataIn;
-			break;
-
-		case 4:
-			addReg[4] = dataIn;
-			break;
-
-		case 5:
-			addReg[5] = dataIn;
-			break;
-
-		case 6:
-			addReg[6] = dataIn;
-			break;
-
-		case 7:
-			addReg[7] = dataIn;
-			break;
-
-		}//end switch
-	}// end read_data
+		sc_uint<3> index = regSelect.read();
+		addReg[index] = dataIn;
+	}//end read_data
 
 	void ADDRESS_REGS::write_data(void){
-			//regSelect.read()
-			switch (regSelect.read()){
-			case 0:
-				dataOut.write(addReg[0]);
-				break;
+				sc_uint<3> index = regSelect.read();
+				dataOut.write(addReg[index]);
 
-			case 1:
-				dataOut.write(addReg[1]);
-				break;
-
-			case 2:
-				dataOut.write(addReg[2]);
-				break;
-
-			case 3:
-				dataOut.write(addReg[3]);
-				break;
-
-			case 4:
-				dataOut.write(addReg[4]);
-				break;
-
-			case 5:
-				dataOut.write(addReg[5]);
-				break;
-
-			case 6:
-				dataOut.write(addReg[6]);
-				break;
-
-			case 7:
-				dataOut.write(addReg[7]);
-				break;
-
-			}//end switch
-		}// end write_data
+	}//end write_data
 
 
 /************************************************
@@ -190,81 +64,18 @@
  * Modified:	N/A
  *
  ***********************************************/
+
 	void INDEX_REGS::read_data(void){
-		//regSelect.read()
-		switch (regSelect.read()){
-		case 0:
-			indexReg[0] = dataIn;
-			break;
-
-		case 1:
-			indexReg[1] = dataIn;
-			break;
-
-		case 2:
-			indexReg[2] = dataIn;
-			break;
-
-		case 3:
-			indexReg[3] = dataIn;
-			break;
-
-		case 4:
-			indexReg[4] = dataIn;
-			break;
-
-		case 5:
-			indexReg[5] = dataIn;
-			break;
-
-		case 6:
-			indexReg[6] = dataIn;
-			break;
-
-		case 7:
-			indexReg[7] = dataIn;
-			break;
-
-		}//end switch
-	}// end read_data
+			sc_uint<3> index = regSelect.read();
+			indexReg[index] = dataIn;
+		}//end read_data
 
 	void INDEX_REGS::write_data(void){
-			//regSelect.read()
-			switch (regSelect.read()){
-			case 0:
-				dataOut.write(indexReg[0]);
-				break;
+					sc_uint<3> index = regSelect.read();
+					dataOut.write(indexReg[index]);
 
-			case 1:
-				dataOut.write(indexReg[1]);
-				break;
+		}//end write_data
 
-			case 2:
-				dataOut.write(indexReg[2]);
-				break;
-
-			case 3:
-				dataOut.write(indexReg[3]);
-				break;
-
-			case 4:
-				dataOut.write(indexReg[4]);
-				break;
-
-			case 5:
-				dataOut.write(indexReg[5]);
-				break;
-
-			case 6:
-				dataOut.write(indexReg[6]);
-				break;
-
-			case 7:
-				dataOut.write(indexReg[7]);
-				break;
-
-			}//end switch
-		}// end write_data
 
 /************************************************
  *
@@ -274,105 +85,18 @@
  * Modified:	N/A
  *
  ***********************************************/
+
 	void FUNC_OP_REGS::read_data(void){
-		//regSelect.read()
-		switch (regSelect.read()){
-		case 0:
-			opReg[0] = dataIn;
-			break;
+				sc_uint<4> index = regSelect.read();
+				opReg[index] = dataIn;
+			}//end read_data
 
-		case 1:
-			opReg[1] = dataIn;
-			break;
+	void FUNC_OP_REGS::write_data(void){
+				sc_uint<4> index = regSelect.read();
+				dataOut.write(opReg[index]);
 
-		case 2:
-			opReg[2] = dataIn;
-			break;
+			}//end write_data
 
-		case 3:
-			opReg[3] = dataIn;
-			break;
-
-		case 4:
-			opReg[4] = dataIn;
-			break;
-
-		case 5:
-			opReg[5] = dataIn;
-			break;
-
-		case 6:
-			opReg[6] = dataIn;
-			break;
-
-		case 7:
-			opReg[7] = dataIn;
-			break;
-
-		case 8:
-			opReg[8] = dataIn;
-			break;
-
-		case 9:
-			opReg[9] = dataIn;
-			break;
-
-		default:
-			std::cout << "ERROR: Inaccessible memory access request";
-			exit (1);
-
-		}//end switch
-	}// end read_data
-
-	void FUNC_OP_REGS::write_data(){
-			//regSelect.read()
-			switch (regSelect.read()){
-			case 0:
-				dataOut.write(opReg[0]);
-				break;
-
-			case 1:
-				dataOut.write(opReg[1]);
-				break;
-
-			case 2:
-				dataOut.write(opReg[2]);
-				break;
-
-			case 3:
-				dataOut.write(opReg[3]);
-				break;
-
-			case 4:
-				dataOut.write(opReg[4]);
-				break;
-
-			case 5:
-				dataOut.write(opReg[5]);
-				break;
-
-			case 6:
-				dataOut.write(opReg[6]);
-				break;
-
-			case 7:
-				dataOut.write(opReg[7]);
-				break;
-
-			case 8:
-				dataOut.write(opReg[8]);
-				break;
-
-			case 9:
-				dataOut.write(opReg[9]);
-				break;
-
-			default:
-				std::cout << "ERROR: Inaccessible memory access request";
-				exit (1);
-
-			}//end switch
-		}// end write_data
 
 	/************************************************
  *
@@ -383,47 +107,98 @@
  *
  ***********************************************/
 	void HOPPER_REGS::read_data(void){
-		//regSelect.read()
-		switch (regSelect.read()){
-		case 0:
-			hopReg[0] = dataIn;
-			break;
-
-		case 1:
-			hopReg[1] = dataIn;
-			break;
-
-		case 2:
-			hopReg[2] = dataIn;
-			break;
-
-		case 3:
-			hopReg[3] = dataIn;
-			break;
-
-		}//end switch
-	}// end read_data
+					sc_uint<2> index = regSelect.read();
+					hopReg[index] = dataIn;
+				}//end read_data
 
 	void HOPPER_REGS::write_data(void){
-			//regSelect.read()
-			switch (regSelect.read()){
-			case 0:
-				dataOut.write(hopReg[0]);
-				break;
+					sc_uint<2> index = regSelect.read();
+					dataOut.write(hopReg[index]);
 
-			case 1:
-				dataOut.write(hopReg[1]);
-				break;
+				}//end write_data
 
-			case 2:
-				dataOut.write(hopReg[2]);
-				break;
 
-			case 3:
-				dataOut.write(hopReg[3]);
-				break;
 
-			}//end switch
-		}// end write_data
+/************************************************
+ *
+ * Function:	FUNCTIONAL UNIT STATUS
+ * Author:		Fred Love
+ * Created:		11/19/15
+ * Modified:	N/A
+ *
+ ***********************************************/
+	void FUNC_UNIT_STATUS::add_request(void){
+		sc_uint<4> index = unitSelect.read();
+		if (statusReg[index]){
+			std::cout << "Unit Busy \n";
+			//exit (1)
+		}
+		else
+			statusReg[index] = 1;
+	}//end add_request
 
+	void FUNC_UNIT_STATUS::output_status(void){
+		sc_uint<4> index = unitSelect.read();
+		outStatus.write(statusReg[index]);
+	}//end output_status
+
+/************************************************
+ *
+ * Function:	GLOBAL OPERAND STATUS
+ * Author:		Fred Love
+ * Created:		11/19/15
+ * Modified:	N/A
+ *
+ ***********************************************/
+
+	void GLOB_OP_STATUS::check_read(void){
+		sc_uint<3> index = regSelect.read();
+		readCountOut.write(statusReg[index].read_count);
+
+	} //end check_read
+
+	void GLOB_OP_STATUS::check_write(void){
+		sc_uint<3> index = regSelect.read();
+		readCountOut.write(statusReg[index].write_count);
+	} //end check_write
+
+	void GLOB_OP_STATUS::request_read(void){
+		sc_uint<3> index = regSelect.read();
+		statusReg[index].read_count = statusReg[index].read_count + 1;
+
+	} //end request_read
+
+	void GLOB_OP_STATUS::request_write(void){
+			sc_uint<3> index = regSelect.read();
+			statusReg[index].write_count = statusReg[index].write_count + 1;
+
+		} //end request_write
+
+	void GLOB_OP_STATUS::read_available(void){
+		sc_uint<3> index = regSelect.read();
+		statusReg[index].read_available = 1;
+
+	}//end read_available
+
+	void GLOB_OP_STATUS::write_available(void){
+		sc_uint<3> index = regSelect.read();
+		statusReg[index] = write_available = 1;
+	}//end write_available
+
+	/*
+	bool GLOB_OP_STATUS::notifyOpComplete(void){
+
+	}//end notifyOpComplete
+	*/
+
+	void GLOB_OP_STATUS::decrement_read(status_node &inNode){
+		inNode.read_count = inNode.read_count - 1;
+	}//end decementCount
+
+	void GLOB_OP_STATUS::decrement_write(status_node &inNode){
+			inNode.write_count = inNode.write_count - 1;
+			if (inNode.write_count == 0){
+				inNode.write_available = 1;
+			}
+		}//end decementCount
 
---
+++ scoreboard.cpp
@@ -0,0 +1,10 @@
+/*
+ * scoreboard.cpp
+ *
+ *  Created on: Nov 27, 2014
+ *      Author: fred
+ */
+
+
+
+
