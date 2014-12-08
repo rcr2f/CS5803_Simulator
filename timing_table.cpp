ad7fde8d04fe5b3dfc03be26bfb71959b6a0de40
rcr2f <rcr2f@mst.edu>
2014-12-08 02:09:07 -0600

    fdffg

--- timing_table.cpp
+++ timing_table.cpp
@@ -16,35 +16,35 @@ Timing_Table::Timing_Table(ostream * out, int num_instr, bool is_CDC6600)
 	m_store = new int[m_num_instr]();
 	m_is6600 = is_CDC6600;
 	
-	*outFile << "issue" << ";";
-	*outFile <<"start" << ";";
-	*outFile << "result" << ";";
-	*outFile << "unit ready" << ";";
-	*outFile << "fetch" << ";";
-	*outFile << "store" << ";";
+	*outFile << "issue" << ",";
+	*outFile <<"start" << ",";
+	*outFile << "result" << ",";
+	*outFile << "unit ready" << ",";
+	*outFile << "fetch" << ",";
+	*outFile << "store" << ",";
 	*outFile << endl;
 }
 
 void Timing_Table::print()
 {
 	for(int i=0; i<m_num_instr; i++) {
-		*outFile << m_issue[i] << ";";
-		*outFile << m_start[i] << ";";
-		*outFile << m_result[i] << ";";
-		*outFile << m_unit_ready[i] << ";";
+		*outFile << m_issue[i] << ",";
+		*outFile << m_start[i] << ",";
+		*outFile << m_result[i] << ",";
+		*outFile << m_unit_ready[i] << ",";
 		if(fetch[i] and not (i==m_num_instr-1)) {
 			if(m_is6600)
-				*outFile << m_result[i]+5 << ";";
+				*outFile << m_result[i]+5 << ",";
 			else
-				*outFile << m_result[i]+4 << ";";
+				*outFile << m_result[i]+4 << ",";
 			}
 		else
 			*outFile <<" ;";
 		if(i==m_num_instr-1){
 			if(m_is6600)
-				*outFile << m_result[i]+5 << ";";
+				*outFile << m_result[i]+5 << ",";
 			else
-				*outFile << m_result[i]+4 << ";";
+				*outFile << m_result[i]+4 << ",";
 			}
 		*outFile << endl;
 	}
